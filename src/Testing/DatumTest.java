e94d1642289bcd45b143f99de201cb02df99fafa
noellamichiels <noella.michiels@telenet.be>
2012-10-01 21:34:27 +0200

    aanmaak klasse DatumTest

---
+++ src/Testing/DatumTest.java
@@ -0,0 +1,19 @@
+package Testing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatumTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

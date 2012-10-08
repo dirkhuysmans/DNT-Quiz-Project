3ac82853984298d5ddb54fff27ccbc378020912a
dirkhuysmans <dirk.huysmans@ace.groept.be>
2012-10-08 19:06:27 +0200

    test gelijktijdige push

--- src/utils/Test.java
+++ src/utils/Test.java
@@ -4,6 +4,9 @@ public class Test {
 
 	/**
 	 * @param args
+	 * @author Noella Michiels
+	 * @author Thijs Maes
+	 * @author Dirk Huysmans
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

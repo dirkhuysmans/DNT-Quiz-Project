d42c948d98e8296168f5c67cc45fbb1bdd68f650
frederikkinnaer <dirk.huysmans@ace.groept.be>
2012-10-08 19:53:09 +0200

    test

--- src/utils/Test.java
+++ src/utils/Test.java
@@ -10,7 +10,9 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test verschillende versies");
+		System.out.println("test verschillende versies1");
+		System.out.println("test verschillende versies2");
 	}
 
+	
 }

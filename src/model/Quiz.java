10fbc43b50c826f23b05db6152ee7c2182e36184
noellamichiels <noella.michiels@telenet.be>
2012-10-15 21:19:22 +0200

    toevoegen package enumKlassen

--- src/model/Quiz.java
+++ src/model/Quiz.java
@@ -1,5 +1,21 @@
 package model;
-
+/**
+ * Deze klasse wordt gebruikt voor het aanmaken van een Quiz
+ * @author Noella
+ *
+ */
 public class Quiz {
+	//
+	// Attributen
+	//
+	private String onderwerp;
+	private int[] leerJaar;
+	private boolean isTest;
+	private boolean isUniekeDeelname;
+	 
+	//
+	// Constructor
+	//
+	
 
 }
---
+++ src/model/enumKlassen/Leraar.java
@@ -0,0 +1,5 @@
+package model.enumKlassen;
+
+public enum Leraar {
+
+}

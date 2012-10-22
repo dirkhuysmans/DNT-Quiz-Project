e03ac2f2450addf9466ffb37eb3d485624b95ae2
thijsmaes <thijsmaes@gmail.com>
2012-10-22 21:09:02 +0200

    aanmaak QuizOpdracht

--- src/model/QuizOpdracht.java
+++ src/model/QuizOpdracht.java
@@ -1,7 +1,37 @@
 package model;
 
 public class QuizOpdracht {
-	private Quiz quiz;
+	private Quiz quiz = new Quiz();
+	private Opdracht opdracht;
 	private int maxScore;
 	
+	public QuizOpdracht (Quiz quiz, Opdracht opdracht, int score){
+		setQuiz(quiz);
+		setOpdracht(opdracht);
+		setMaxScore(score);
+	}
+
+	public Quiz getQuiz() {
+		return quiz;
+	}
+
+	public void setQuiz(Quiz quiz) {
+		this.quiz = quiz;
+	}
+
+	public Opdracht getOpdracht() {
+		return opdracht;
+	}
+
+	public void setOpdracht(Opdracht opdracht) {
+		this.opdracht = opdracht;
+	}
+
+	public int getMaxScore() {
+		return maxScore;
+	}
+
+	public void setMaxScore(int maxScore) {
+		this.maxScore = maxScore;
+	}
 }

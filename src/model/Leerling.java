03d140f149fe4123302b486cd3cd3cf6acbc66e8
dirkhuysmans <dirk.huysmans@ace.groept.be>
2012-10-25 21:59:20 +0200

    klasse Leerling en LeerlingContainer toegevoegd.

---
+++ src/model/Leerling.java
@@ -0,0 +1,85 @@
+package model;
+
+
+/**
+ * 
+ * @author Dirk Huysmans
+ *
+ */
+
+public class Leerling {
+	
+/**
+ *  attributen	
+ */
+	
+private String leerlingNaam;
+private int leerjaar;
+
+//constructor
+public Leerling(String leerlingNaam, int leerjaar) {
+	this.leerlingNaam = leerlingNaam;
+	this.leerjaar = leerjaar;
+}
+
+//setters
+public void setLeerlingNaam(String leerlingNaam) {
+	this.leerlingNaam = leerlingNaam;
+}
+
+public void setLeerjaar(int leerjaar) {
+	this.leerjaar = leerjaar;
+}
+
+
+public String getLeerlingNaam() {
+	return leerlingNaam;
+}
+
+public int getLeerjaar() {
+	return leerjaar;
+}
+
+@Override
+public int hashCode() {
+	final int prime = 31;
+	int result = 1;
+	result = prime * result + leerjaar;
+	result = prime * result
+			+ ((leerlingNaam == null) ? 0 : leerlingNaam.hashCode());
+	return result;
+}
+
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Leerling other = (Leerling) obj;
+	if (leerjaar != other.leerjaar)
+		return false;
+	if (leerlingNaam == null) {
+		if (other.leerlingNaam != null)
+			return false;
+	} else if (!leerlingNaam.equals(other.leerlingNaam))
+		return false;
+	return true;
+}
+
+/**
+ * @param Naam en leerjaar
+ */
+@Override
+public String toString() {
+	return "Naam: " + leerlingNaam + ", leerjaar=" + leerjaar;
+}
+
+
+
+
+
+
+}
---
+++ src/model/LeerlingContainer.java
@@ -0,0 +1,103 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeerlingContainer {
+
+	/**
+	 * @author Dirk Huysmans
+	 */
+	
+	private List<Leerling> LeerlingContainer = new ArrayList<Leerling>();
+	
+	/**
+	 * Voegt een leerling toe, indien deze nog niet in de lijst opgenomen is
+	 * @throws Exception 
+	 */
+	
+	public void voegLeerlingToe(Leerling leerling) throws Exception{
+		try{
+			if(LeerlingContainer.contains(leerling)){
+				throw new Exception("Deze leerling bestaat al in de lijst.");
+			}
+			else{
+				LeerlingContainer.add(leerling);
+			}
+		}
+		catch (Exception e)
+		{
+			throw new Exception(e.getMessage());
+		
+		}
+		
+	}
+	
+	
+	/**
+	 * Verwijder een leerling
+	 */
+	public void verwijderLeerling(Leerling leerling) throws Exception{
+		try{
+			if(LeerlingContainer.contains(leerling)){
+				LeerlingContainer.remove(leerling);
+			}
+			else{
+				throw new Exception("Leerling werd niet in de lijst gevonden en kan dan ook niet verwijderd worden.");
+			}
+		}
+		catch (Exception e){
+			throw new Exception(e.getMessage());
+		}
+		
+		
+	}
+	
+	/**
+	 * Wijzig  naam leerling
+	 */
+	public void wijzigNaamLeerling(Leerling leerling, String nieuweNaam) throws Exception{
+		try{
+			if(LeerlingContainer.contains(leerling)){
+				leerling.setLeerlingNaam(nieuweNaam);
+			}
+			else{
+				throw new Exception("Leerling niet gevonden in de lijst. Naam kan bijgevolg niet aangepast worden.");
+			}
+		}
+		catch (Exception e){
+			throw new Exception(e.getMessage());
+		}
+	}
+
+	/**
+	 * Wijzig leerjaar van leerling
+	 */
+	public void wijzigKlasVanLeerling(Leerling leerling, int nieuwLeerjaar) throws Exception{
+		try{
+			if(LeerlingContainer.contains(leerling)){
+				leerling.setLeerjaar(nieuwLeerjaar);
+			}
+			else{
+				throw new Exception("Leerling niet gevonden in de lijst. Leerjaar voor deze leerling kan bijgevolg niet aangepast worden.");
+			}
+		}
+		catch (Exception e){
+			throw new Exception(e.getMessage());
+		}
+	}
+	
+	@Override
+	public String toString() {
+		String LijstLeerlingen ="";
+		for (Leerling leerling : LeerlingContainer) {
+			LijstLeerlingen += leerling + "/n";
+		}
+		if(LijstLeerlingen==""){
+			LijstLeerlingen = "De lijst met leerlingen is leeg.";
+		}
+				
+		return LijstLeerlingen;
+	}
+	
+}
